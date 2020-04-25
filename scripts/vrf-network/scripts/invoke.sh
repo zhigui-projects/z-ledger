a0b5f887b1fdf633230ff31b390f2090689f5f9a
BeDreamCoder <tailinzhang1993@gmail.com>
2020-04-25 18:51:11 +0800

    refine scripts

---
+++ scripts/vrf-network/scripts/invoke.sh
@@ -0,0 +1,110 @@
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Build your first network (BYFN) end-to-end test"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+CC_SRC_LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+NO_CHAINCODE="$6"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${CC_SRC_LANGUAGE:="go"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+: ${NO_CHAINCODE:="false"}
+CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=20
+PACKAGE_ID=""
+
+if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ]; then
+	CC_RUNTIME_LANGUAGE=golang
+	CC_SRC_PATH="github.com/hyperledger/fabric-samples/chaincode/abstore/go/"
+elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
+	CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/javascript/"
+elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
+	CC_RUNTIME_LANGUAGE=java
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/java/"
+else
+	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
+	echo Supported chaincode languages are: go, javascript, java
+	exit 1
+fi
+
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	setGlobals 0 1
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+joinChannel () {
+	for org in 1 2 3 4 5; do
+	    for peer in 0 1; do
+		joinChannelWithRetry $peer $org
+		echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
+		sleep $DELAY
+		echo
+	    done
+	done
+}
+
+
+if [ "${NO_CHAINCODE}" != "true" ]; then
+	# Invoke chaincode on peer0.org1 and peer0.org2
+	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+	chaincodeInvoke 0 0 1 1 1 0 2 1 2 0 3 1 3 0 4 1 4 0 5 1 5
+
+	# Query chaincode on peer0.org1
+#	echo "Querying chaincode on peer0.org1..."
+#	chaincodeQuery 0 1 90
+fi
+
+echo
+echo "========= All GOOD, BYFN execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
