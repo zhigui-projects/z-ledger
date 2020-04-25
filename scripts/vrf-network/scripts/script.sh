eddcbd1b3ddac24987ed259583d3829cb9cbf726
BeDreamCoder <tailinzhang1993@gmail.com>
2020-04-25 18:33:15 +0800

    refine scripts

--- scripts/vrf-network/scripts/script.sh
+++ scripts/vrf-network/scripts/script.sh
@@ -97,6 +97,12 @@ echo "Updating anchor peers for org1..."
 updateAnchorPeers 0 1
 echo "Updating anchor peers for org2..."
 updateAnchorPeers 0 2
+echo "Updating anchor peers for org3..."
+updateAnchorPeers 0 3
+echo "Updating anchor peers for org4..."
+updateAnchorPeers 0 4
+echo "Updating anchor peers for org5..."
+updateAnchorPeers 0 5
 
 if [ "${NO_CHAINCODE}" != "true" ]; then
 
@@ -108,35 +114,41 @@ if [ "${NO_CHAINCODE}" != "true" ]; then
 	installChaincode 0 1
 	echo "Install chaincode on peer0.org2..."
 	installChaincode 0 2
+	echo "Install chaincode on peer0.org3..."
+	installChaincode 0 3
+	echo "Install chaincode on peer0.org4..."
+	installChaincode 0 4
+	echo "Install chaincode on peer0.org5..."
+	installChaincode 0 5
+
+	## Install chaincode on peer1.org1
+	echo "Installing chaincode on peer1.org1..."
+	installChaincode 1 1
+	echo "Installing chaincode on peer1.org2..."
+	installChaincode 1 2
+	echo "Installing chaincode on peer1.org3..."
+	installChaincode 1 3
+	echo "Installing chaincode on peer1.org4..."
+	installChaincode 1 4
+	echo "Installing chaincode on peer1.org5..."
+	installChaincode 1 5
 
 	## query whether the chaincode is installed
 	queryInstalled 0 1
 
 	## approve the definition for org1
 	approveForMyOrg 1 0 1
-
-	## check whether the chaincode definition is ready to be committed
-    ## expect org1 to have approved and org2 not to
-	checkCommitReadiness 1 0 1 "\"Org1MSP\": true" "\"Org2MSP\": false"
-	checkCommitReadiness 1 0 2 "\"Org1MSP\": true" "\"Org2MSP\": false"
-
 	## now approve also for org2
 	approveForMyOrg 1 0 2
-
-	## check whether the chaincode definition is ready to be committed
-	## expect them both to have approved
-	checkCommitReadiness 1 0 1 "\"Org1MSP\": true" "\"Org2MSP\": true"
-	checkCommitReadiness 1 0 2 "\"Org1MSP\": true" "\"Org2MSP\": true"
+	approveForMyOrg 1 0 3
+	approveForMyOrg 1 0 4
+	approveForMyOrg 1 0 5
 
 	## now that we know for sure both orgs have approved, commit the definition
-	commitChaincodeDefinition 1 0 1 0 2
-
-	## query on both orgs to see that the definition committed successfully
-	queryCommitted 1 0 1
-	queryCommitted 1 0 2
+	commitChaincodeDefinition 1 0 1 0 2 0 3 0 4 0 5
 
 	# invoke init
-	chaincodeInvoke 1 0 1 0 2
+	chaincodeInvoke 1 0 1 1 1 0 2 1 2 0 3 1 3 0 4 1 4 0 5 1 5
 
 	# Query chaincode on peer0.org1
 	echo "Querying chaincode on peer0.org1..."
@@ -144,15 +156,11 @@ if [ "${NO_CHAINCODE}" != "true" ]; then
 
 	# Invoke chaincode on peer0.org1 and peer0.org2
 	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
-	chaincodeInvoke 0 0 1 0 2
+	chaincodeInvoke 0 0 1 1 1 0 2 1 2 0 3 1 3 0 4 1 4 0 5 1 5
 
 	# Query chaincode on peer0.org1
 	echo "Querying chaincode on peer0.org1..."
 	chaincodeQuery 0 1 90
-
-	## Install chaincode on peer1.org2
-	echo "Installing chaincode on peer1.org2..."
-	installChaincode 1 2
 
 	# Query on chaincode on peer1.org2, check if the result is 90
 	echo "Querying chaincode on peer1.org2..."
