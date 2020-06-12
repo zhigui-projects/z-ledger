fcebbbc71b2b786b2934256eb04bb574ad9a5181
BeDreamCoder <tailinzhang1993@gmail.com>
2020-06-12 16:55:13 +0800

    modify configtxgen tools for hotstuff

--- common/channelconfig/util.go
+++ common/channelconfig/util.go
@@ -8,6 +8,7 @@ package channelconfig
 
 import (
 	"fmt"
+	hs "github.com/hyperledger/fabric-protos-go/orderer/hotstuff"
 	"io/ioutil"
 	"math"
 
@@ -294,6 +295,33 @@ func MarshalEtcdRaftMetadata(md *etcdraft.ConfigMetadata) ([]byte, error) {
 	for _, c := range copyMd.Consenters {
 		// Expect the user to set the config value for client/server certs to the
 		// path where they are persisted locally, then load these files to memory.
+		clientCert, err := ioutil.ReadFile(string(c.GetClientTlsCert()))
+		if err != nil {
+			return nil, fmt.Errorf("cannot load client cert for consenter %s:%d: %s", c.GetHost(), c.GetPort(), err)
+		}
+		c.ClientTlsCert = clientCert
+
+		serverCert, err := ioutil.ReadFile(string(c.GetServerTlsCert()))
+		if err != nil {
+			return nil, fmt.Errorf("cannot load server cert for consenter %s:%d: %s", c.GetHost(), c.GetPort(), err)
+		}
+		c.ServerTlsCert = serverCert
+	}
+	return proto.Marshal(copyMd)
+}
+
+// MarshalHotStuffMetadata serializes hotstuff metadata.
+func MarshalHotStuffMetadata(md *hs.ConfigMetadata) ([]byte, error) {
+	copyMd := proto.Clone(md).(*hs.ConfigMetadata)
+	for _, c := range copyMd.Consenters {
+		// Expect the user to set the config value for client/server certs to the
+		// path where they are persisted locally, then load these files to memory.
+		signCert, err := ioutil.ReadFile(string(c.GetSignCert()))
+		if err != nil {
+			return nil, fmt.Errorf("cannot load sign cert for consenter %s:%d: %s", c.GetHost(), c.GetPort(), err)
+		}
+		c.SignCert = signCert
+
 		clientCert, err := ioutil.ReadFile(string(c.GetClientTlsCert()))
 		if err != nil {
 			return nil, fmt.Errorf("cannot load client cert for consenter %s:%d: %s", c.GetHost(), c.GetPort(), err)
