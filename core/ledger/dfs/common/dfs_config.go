31b5713da75b6993a2554c259ed4acae135303b9
Max Peng <pengisgood@gmail.com>
2020-06-12 11:10:24 +0800

    add comments
    Signed-off-by: Max Peng <pengisgood@gmail.com>

--- core/ledger/dfs/common/dfs_config.go
+++ core/ledger/dfs/common/dfs_config.go
@@ -1,17 +1,24 @@
 package common
 
 type Config struct {
+	// dfs type, supported values: hdfs, ipfs
 	Type     string
 	HdfsConf *HdfsConfig
 	IpfsConf *IpfsConfig
 }
 
 type HdfsConfig struct {
-	User                string
-	NameNodes           []string
+	// HDFS user
+	User string
+
+	// HDFS namenode addresses
+	NameNodes []string
+
+	// Docker DNS config is needed when this value is true
 	UseDatanodeHostname bool
 }
 
 type IpfsConfig struct {
+	// IPFS api url, example: 127.0.0.1:5001
 	Url string
 }
